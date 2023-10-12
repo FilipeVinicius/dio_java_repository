aae32779fa457132555529c622821be613aa78ba
FilipeVinicius <filipevinicius47@gmail.com>
2023-10-12 19:45:18 -0300

    Update

---
+++ DesafiosList/src/ListaTarefas.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+    //atributo
+    private List<Tarefa> tarefaList; 
+
+    public ListaTarefas() {
+        this.tarefaList  = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa tarefa : tarefaList) {
+            if (tarefa.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(tarefa);
+            } 
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+
+        listaTarefas.adicionarTarefa("Tarefa 1");
+       
+    }
+
+}
