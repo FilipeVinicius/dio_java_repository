90569a61466ff59f04d432cab506bf928fa35144
FilipeVinicius <filipevinicius47@gmail.com>
2023-10-20 19:43:22 -0300

    Desafio_Iphone

---
+++ Iphone/Iphone/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Iphone novoIphone = new Iphone();
+
+        System.out.println("##### Chamada #####");
+        novoIphone.ligar();
+        novoIphone.atender();
+        novoIphone.iniciarCorreioDeVoz();
+
+        System.out.println("##### Navegação #####");
+        novoIphone.selecionarMusica();
+        novoIphone.tocar();
+        novoIphone.pausar();
+
+        System.out.println("##### Reproduzir Música #####");
+        novoIphone.adicionarNovaAba();
+        novoIphone.exibirPagina();
+        novoIphone.atualizarPagina();
+    }
+}
